abaa84435b2cfeee295c28b07dcb3fc685fabde4
Samuel310 <samuelbenjamin310@gmail.com>
2023-04-15 17:37:09 +0530

    updated header file

---
+++ ios/Classes/RecaptchaEnterprise-Bridging-Header.h
@@ -0,0 +1,13 @@
+//
+//  RecaptchaEnterprise-Bridging-Header.h
+//  flutter_grecaptcha
+//
+//  Created by Samuel I on 15/04/23.
+//
+
+#ifndef RecaptchaEnterprise_Bridging_Header_h
+#define RecaptchaEnterprise_Bridging_Header_h
+
+#import <RecaptchaEnterprise/RecaptchaEnterprise.h>
+
+#endif /* RecaptchaEnterprise_Bridging_Header_h */
